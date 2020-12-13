8c1f9e2fea5f3da96f12a65e46c881c1d2b59a14
Naman <46571853+np453@users.noreply.github.com>
2020-12-14 01:54:08 +0530

    minor changes

--- client/src/components/common/cube.jsx
+++ client/src/components/common/cube.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import img from '../../assets/works_old.jpg'
+import logo from '../../assets/logo.png'
 export default class Cube extends Component {
     state = {
         classVal: "cube"
@@ -9,12 +10,12 @@ export default class Cube extends Component {
             <div className="container d-flex justify-content-center">
                 <div class="scene">
                     <div class={this.state.classVal}>
-                        <div class="cube__face cube__face--front"><img src={img} className="img img-fluid" alt=""/></div>
-                        <div class="cube__face cube__face--back">back</div>
-                        <div class="cube__face cube__face--right">right</div>
-                        <div class="cube__face cube__face--left">left</div>
-                        <div class="cube__face cube__face--top">top</div>
-                        <div class="cube__face cube__face--bottom">bottom</div>
+                        <div class="cube__face cube__face--front"><img src={logo} className="img img-fluid" alt=""/></div>
+                        <div class="cube__face cube__face--back">Before</div>
+                        <div class="cube__face cube__face--right">Service</div>
+                        <div class="cube__face cube__face--left">Self</div>
+                        <div class="cube__face cube__face--top"> </div>
+                        <div class="cube__face cube__face--bottom"> </div>
                     </div>
                 </div>
             </div>
